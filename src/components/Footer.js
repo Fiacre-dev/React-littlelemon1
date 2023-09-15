d0d4e2768ee7ade0118edbe1370bb9091dfd87e2
Fiacre-dev <fiacredev@gmail.com>
2023-09-15 16:42:01 +0100

    Mise à jour des fichiers package et App.js, ajout de fichiers dans src/components/

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,48 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer-content">
-        <p>Contactez-nous : contact@littlelemon.com</p>
-        <ul>
-          <li><a href="/a-propos">À propos de nous</a></li>
-          <li><a href="/politique-de-confidentialite">Politique de confidentialité</a></li>
-          <li><a href="/mentions-legales">Mentions légales</a></li>
-        </ul>
-      </div>
-      <div className="social-icons">
-        <a href="#" className="icon-link"><i className="fab fa-facebook"></i></a>
-        <a href="#" className="icon-link"><i className="fab fa-twitter"></i></a>
-        <a href="#" className="icon-link"><i className="fab fa-instagram"></i></a>
+    <footer className='sect-footer'>
+      <div className='footer-element'>
+        <div className='footer-img'>
+          <img src="img.jpg" alt="image footer" />
+        </div>
+        <div className='footer-nav'>
+          <h1>Doormat</h1>
+          <ul>
+            <li>Navigation</li>
+            <li>Accueil</li>
+            <li>A propos</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Commander en ligne</li>
+            <li>Se connecter</li>
+          </ul>
+        </div>
+        <div className='footer-contact'>
+          <h1>Contact</h1>
+          <ul>
+            <li>Adresse</li>
+            <li>Numéro de téléphone</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div className='footer-sociaux'>
+          <h1>Réseaux sociaux</h1>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/votrepage" target="_blank" rel="noopener noreferrer">Facebook</a>
+            </li>
+            <li>
+              <a href="https://twitter.com/votrepage" target="_blank" rel="noopener noreferrer">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/votrepage" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </li>
+            {/* Ajoutez d'autres liens vers des réseaux sociaux si nécessaire */}
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,110 @@
 import React from 'react';
+import './hero.css';
+import './promotions.css';
+import './temoignages.css';
+import './a-propos.css';
 
 function Main() {
   return (
     <main>
-      {/* Contenu de l'en-tête */}
+       {/* Section Héros */}
+       <section className="hero">
+        <div className='Hero-element'>
+          <div>
+          <h1>Little lemon</h1>
+          <h5>Chicago</h5>
+          <p>Une brieve description du type de nourriture qu'il propose.</p>
+         <div className='bouton'>
+            <a href='#' className='btn-hero'>Réserver une table</a>
+         </div>
+          </div>
+          <div className='hero2'>
+            <img src="./restauranfood.jpg" alt="Image-hero"/>
+          </div>
+        </div>
+      </section>
+
+      {/* Section Saillants */}
+      <section className="promotion">
+        <div className='promo-title'>
+          <h2>Promotions</h2>
+          <div className='bouton2'><a href="#" className='btn'>Commander en ligne</a></div>
+        </div>
+        <div className='promo-element'>
+          <div>
+            <img src="greek-salad.jpg" alt="Greek salad"/>
+            <h2>salade grecque</h2>
+            <span>$12.99</span>
+            <p>Description du plat</p>
+            <a href="#" className='btn'>Commander une livraison</a>
+          </div>
+          <div>
+            <img src="greek-salad.jpg" alt="Greek salad"/>
+            <h2>Bruchecta</h2>
+            <span>$5.99</span>
+            <p>Description du plat</p>
+            <a href="#" className='btn'>Commander une livraison</a>
+          </div>
+          <div>
+            <img src="greek-salad.jpg" alt="Greek salad"/>
+            <h2>Dessert au citron</h2>
+            <span>$3.99</span>
+            <p>Description du plat</p>
+            <a href="#" className='btn'>Commander une livraison</a>
+          </div>
+        </div>
+      </section>
+
+      {/* Section Témoignages */}
+      <section className="temoignages">
+        <h1>Laisser  des avis et de notes sur 5 etoiles</h1>
+        <div className='temoignages-personne'>
+          <div className='content'>
+            <h4>Name</h4>
+            <div>
+             <img src="comment1.jpg" alt="personne commenter"/>
+            </div>
+            <p>Avis</p>
+          </div>
+          <div className='content'>
+            <h4>Name</h4>
+            <div>
+              <img src="comment2.jpg" alt="personne commenter"/>
+            </div>
+            <p>Avis</p>
+          </div>
+          <div className='content'>
+            <h4>Name</h4>
+            <div>
+             <img src="comment3.jpg" alt="personne commenter"/>
+            </div>
+            <p>Avis</p>
+          </div>
+          <div  className='content'>
+            <h4>Name</h4>
+            <div>
+              <img src="comment4.jpg" alt="personne commenter"/>
+            </div>
+            <p>Avis</p>
+          </div>
+        </div>
+        {/* Ajoutez plus de témoignages au besoin */}
+      </section>
+
+      {/* Section À Propos */}
+      <section className="a-propos">
+        <div className='a-propos-sect'>
+          <div>
+            <h1>Little lemon</h1>
+            <h4>Chicago</h4>
+            <p>Une brieve description du type de nourriture qu'il propose.</p>
+          </div>
+          <div>
+            <img src="resta.jpg" alt="Image1"/>
+            <img src="resnfood.jpg" alt="image2"/>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
